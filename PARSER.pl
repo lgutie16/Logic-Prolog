e4911b193174f19e3cdab32018183f228345f450
lgutie16 <lgutie16.eafit.edu.co>
2016-11-02 19:48:32 -0500

    Better Parser :)

--- PARSER.pl
+++ PARSER.pl
@@ -1,58 +1,77 @@
-addList([], L, L).
-addLlist([H|T], L, L1) :- add(H, L2, L1), addList(T, L, L2).
+mergeList([],[],[]).
+mergeList([X],[],[X]).
+mergeList([],[Y],[Y]).
+mergeList([X|List1],[Y|List2],[X,Y|List]) :- mergeList(List1,List2,List).
+
+
+herbrand(File) :-
+      seeing(Old),      /* save for later */
+      see(user),
+      tell('herbrand.txt'),
+      write('['),
+      see(File),
+      repeat,
+      read(Term),
+        ( Term == end_of_file -> true ;
+          Term =..Lista,
+          mergeList(Lista, Lista, L),
+          write(L)
+      ),
+      seen,
+      write(']'),nl,
+      told,
+      see(Old).
 
 
 
 browse(File) :-
-		write('Enter name of file to save list'), read(List),         
-        seeing(Old),      /* save for later */ 
+       write('Enter name of file to save list'), read(List),
+        seeing(Old),      /* save for later */
         see(user),
-        
-        tell(List), 
+
+        tell(List),
         write('['),
-        see(File), 	        
-	    repeat,
-	    read(Term),
-		    ( Term == end_of_file -> true ;	
-		      Term =..Lista,
-		      arg(1, Lista, S),    
-		      (S == ':-' -> 
-		      arg(1, Term, M),	
-		      arg(2, Term, N),
-		      write('['),
-		      write('['),      	 
-		      write(M),
-		      write(']'),
-		      write(', ['),
-		      write(N),
-		      write('] ],'), nl, fail;
-		      write('['), 
-		      Term =..E,
-		      write(Term),
-		      write('],'),
-		      tell('herbran.txt'),
-		      write(E), fail	      
-		    )
+        see(File),
+      repeat,
+      read(Term),
+        ( Term == end_of_file -> true ;
+          Term =..Lista,
+          arg(1, Lista, S),
+          (S == ':-' ->
+          arg(1, Term, M),
+          arg(2, Term, N),
+          write('['),
+          write('['),
+          write(M),
+          write(']'),
+          write(', ['),
+          write(N),
+          write('] ],'), nl, fail;
+          write('['),
+          Term =..E,
+          write(Term),
+          write('],'),
+          tell('herbran.txt'),
+          write(E), fail
+        )
 
-	    ),
-	    seen,
-	    told ,	    
-	    write('[]'), nl,
-	    write(']'),
-	   
-	    told, 	    
-	    see(Old). 
+      ),
+      seen,
+      told ,
+      write('[]'), nl,
+      write(']'),
+      told,
+      see(Old).
 
-make_file :-  
-       telling(Old),      /* current write output */      
-       write('Enter name of file to browse: '), read(File), 
-       tell(File),      /* open this file */ 
+make_file :-
+       telling(Old),      /* current write output */
+       write('Enter name of file to browse: '), read(File),
+       tell(File),      /* open this file */
        repeat,
-	   read(Term),
-	   ( Term == stop -> true ; 
-	     write(Term), nl, fail	
-	   ),
-       told,              /* close ToFile */ 
+       read(Term),
+       ( Term == stop -> true ;
+         write(Term), nl, fail
+       ),
+       told,              /* close ToFile */
        tell(Old),       /* resume this output */
        browse(File).
-
--- eval_arithmetic_expression_minus.pl
+++ eval_arithmetic_expression_minus.pl
@@ -1,11 +1,9 @@
 eval(A+B,CV):-eval(A,AV),eval(B,BV),CV is AV+BV.
 eval(A-B,CV):-eval(A,AV),eval(B,BV),CV is AV-BV.
-<<<<<<< HEAD
 eval(A*B,CV):-eval(A,AV),eval(B,BV),CV is AV*BV.
 eval(Num,Num):-number(Num).
 eval(Letter,Letter) :- string(Letter).
 eval(Num,Letter):-number(Letter).
-=======
 eval(Num,Num):-number(Num).
 eval(Letter,Letter) :- string(Letter).
->>>>>>> 08afc6d5305078212ec95a1280c695fde5f49a99
+
